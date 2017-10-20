34e0e257d7167a6c81f7a79c81070a8cd6e8ed38
Shikanime <shikanime@hotmail.fr>
2017-10-20 23:35:05 +0200

    fix validator

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -48,16 +48,19 @@ const signin = (deps, models, configs) => async (req, res, next) => {
 };
 
 const signup = (deps, models, configs) => async (req, res) => {
-  req.assert('email', 'Email is not valid').isEmail();
-  req.assert('password', 'Password must be at least 4 characters long').len({ min: 4 });
+  req.assert('email', configs.payload.format.emailBadFormat).isEmail();
+  req.assert('email', configs.payload.format.emailEmpty).notEmpty();
+  req.assert('password', configs.payload.format.passwordLenght).isLength({ min: 4, max: 16 });
   req.sanitize('email').normalizeEmail({ gmail_remove_dots: false });
 
   const errors = req.validationErrors();
 
   if (errors.length) {
+    const allErrors = errors.map(e => e.msg);
+
     res.json({
       success: false,
-      payload: configs.payload.badFormat,
+      payload: allErrors[0],
     });
     return;
   }
@@ -109,8 +112,8 @@ const getUserData = (deps, models, configs) => (req, res, next) => {
 };
 
 export default (deps, models, configs) => (router) => {
+  router.get('/', getUserData(deps, models, configs));
   router.post('/signin', signin(deps, models, configs));
   router.post('/signup', signup(deps, models, configs));
-  router.post('/', getUserData(deps, models, configs));
   return router;
 };
