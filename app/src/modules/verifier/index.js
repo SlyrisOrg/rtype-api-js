0f62e90c0b7465ce0abc134a52f98d139a3ab82a
Shikanime <shikanime@hotmail.fr>
2017-12-02 01:01:49 +0100

    /\transfert patch/"

--- app/src/modules/verifier/index.js
+++ app/src/modules/verifier/index.js
@@ -3,11 +3,18 @@ import profileVerifier from "./profile";
 import shipVerifier from "./ship";
 
 const allVerifier = deps =>
-  async (data) => ({
-    ...await userVerifier(deps)(data),
-    profile: await profileVerifier(deps)(data.profile),
-    ship: await shipVerifier(deps)(data.ship),
-  });
+  async (data) => {
+
+    const user = data && await userVerifier(deps)(data);
+    const profile = data.profile && await profileVerifier(deps)(data.profile);
+    const ship = data.ship && await shipVerifier(deps)(data.ship);
+
+    return {
+      ...user,
+      profile,
+      ship,
+    };
+  };
 
 export default deps => ({
   all: allVerifier(deps),
